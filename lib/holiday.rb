6946c0a722c8e22058b866c24d96a9bf769ce87e
adam n <adam.nawroc@gmail.com>
2018-03-02 16:34:25 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,8 +39,13 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  # holiday_hash[:winter].each do |k, v|
+  #   # binding.pry
+  #
+  #   holiday_hash[:winter][k] << supply
+  #   # k << supply
+  # end
 
-  
 
 end
 
